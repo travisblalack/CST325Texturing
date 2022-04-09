4b4e2a46ffec93d057358861c2c41a9104faa747
Travis Blalack <travisblalack@gmail.com>
2022-04-09 16:03:27 -0700

    Add files via upload

---
+++ Week 10 - Texturing, Transparency/shaders/unlit.textured.fs.glsl
@@ -0,0 +1,32 @@
+precision mediump float;
+
+uniform sampler2D uTexture;
+uniform float uAlpha;
+
+
+
+// todo #3 - receive texture coordinates and verify correctness by 
+// using them to set the pixel color 
+
+
+varying vec2 vTexcoords;
+
+void main(void) {
+   
+
+
+    // todo #5
+   
+
+   gl_FragColor = texture2D(uTexture,vTexcoords);
+
+ 
+   gl_FragColor = vec4(vTexcoords,0.0,1.0);
+    // todo #3
+    
+
+
+  
+}
+
+// EOF 00100001-10
